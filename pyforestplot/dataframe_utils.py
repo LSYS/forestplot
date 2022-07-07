a45601ce228646395e3e4e8baf4583db984d7991
Lucas Shen Y. S <lucas@lucasshen.com>
2022-07-07 23:56:50 +0800

    remove unnecessary vertical spacing

--- pyforestplot/dataframe_utils.py
+++ pyforestplot/dataframe_utils.py
@@ -3,11 +3,9 @@ from typing import Optional
 
 
 def insert_groups(
-	dataframe: pd.core.frame.DataFrame,
-	groupvar: str,
-	varlabel: str
+    dataframe: pd.core.frame.DataFrame, groupvar: str, varlabel: str
 ) -> pd.core.frame.DataFrame:
-	"""
+    """
 	Insert the name of variable groups as a psuedo variable in the dataframe.
 
 	Parameters
@@ -24,33 +22,31 @@ def insert_groups(
 	-------
 		pd.core.frame.DataFrame with group variable labels inserted as psuedo variables.
 	"""
-	
-	df_groupsasvar = pd.DataFrame()
+    df_groupsasvar = pd.DataFrame()
+    for group in dataframe[groupvar].unique():
+        _df = dataframe.query(f"{groupvar}==@group")
+        addgroupvar = pd.DataFrame({varlabel: [group], groupvar: [group]})
+        df_groupsasvar = pd.concat(
+            [df_groupsasvar, addgroupvar, _df], ignore_index=True
+        )
 
-	for group in dataframe[groupvar].unique():
-		_df = dataframe.query(f'{groupvar}==@group')
-		addgroupvar = pd.DataFrame({varlabel: [group], groupvar: [group]})
-		df_groupsasvar = pd.concat([df_groupsasvar, addgroupvar, _df], ignore_index=True)
-
-
-	return df_groupsasvar
+    return df_groupsasvar
 
 
 def sort_data(
-	dataframe: pd.core.frame.DataFrame,
-	estimate: str,
-	groupvar: str,
-	sort: bool = False,
-	sortby: Optional[str] = None,
-	sortascend: bool = True,
-	**kwargs
+    dataframe: pd.core.frame.DataFrame,
+    estimate: str,
+    groupvar: str,
+    sort: bool = False,
+    sortby: Optional[str] = None,
+    sortascend: bool = True,
+    **kwargs,
 ) -> pd.core.frame.DataFrame:
-	"""
+    """
 	Sort the dataframe according to the stated options.
 
 	Parameters
 	----------
-
 	dataframe (pandas.core.frame.DataFrame)
 		Pandas DataFrame where rows are variables. Columns are variable name, estimates,
 		margin of error, etc.
@@ -70,27 +66,26 @@ def sort_data(
 	-------
 		pd.core.frame.DataFrame that is sorted by the stated options.
 	"""
-	
-	if sort or (sortby is not None):
-		if sortby is None:
-			sortby = estimate
+    if sort or (sortby is not None):
+        if sortby is None:
+            sortby = estimate
 
-		if groupvar is not None:
-			dataframe.sort_values([groupvar, sortby], ascending=sortascend, inplace=True)
-		else:
-			dataframe.sort_values(sortby, ascending=sortascend, inplace=True)
+        if groupvar is not None:
+            dataframe.sort_values(
+                [groupvar, sortby], ascending=sortascend, inplace=True
+            )
+        else:
+            dataframe.sort_values(sortby, ascending=sortascend, inplace=True)
 
-		dataframe.reset_index(drop=True, inplace=True)
+        dataframe.reset_index(drop=True, inplace=True)
 
-	return dataframe
+    return dataframe
 
 
-def reverse_dataframe(
-	dataframe: pd.core.frame.DataFrame
-) -> pd.core.frame.DataFrame:
-	"""Flip the dataframe so that last row is now first and so on."""
-	dataframe = dataframe[::-1]
-	return dataframe.reset_index(drop=True)	
+def reverse_dataframe(dataframe: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+    """Flip the dataframe so that last row is now first and so on."""
+    dataframe = dataframe[::-1]
+    return dataframe.reset_index(drop=True)
 
 
 def load_data(name: str, **param_dict) -> pd.core.frame.DataFrame:
@@ -111,11 +106,8 @@ def load_data(name: str, **param_dict) -> pd.core.frame.DataFrame:
 	-------
 	pd.core.frame.DataFrame.
 	"""
-
     available_data = ["mortality"]
-
     name = name.lower().strip()
-
     if name in available_data:
         url = f"https://raw.githubusercontent.com/lsys/pyforestplot/main/examples/data/{name}.csv"
         df = pd.read_csv(url, **param_dict)
