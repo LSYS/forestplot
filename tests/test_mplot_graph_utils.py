22faae91d82f4d112242076a1825254c3b69c47d
LSYS <lucas@lucasshen.com>
2023-12-16 12:58:38 +0800

    Add test for mdraw_legend (#88, #89)

--- tests/test_mplot_graph_utils.py
+++ tests/test_mplot_graph_utils.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.pyplot import Axes
+from matplotlib.lines import Line2D
 
 from forestplot.mplot_graph_utils import (
     mdraw_ci,
     mdraw_est_markers,
     mdraw_ref_xline,
-    mdraw_yticklabels,
+    mdraw_yticklabels, mdraw_legend  
 )
 
 x, y = [0, 1, 2], [0, 1, 2]
@@ -95,3 +96,36 @@ def test_mdraw_ci():
     # Assertions
     assert isinstance(ax, Axes)
     assert len(ax.collections) == len(set(models_vector))
+
+
+def test_mdraw_legend():
+    # Create a simple plot
+    fig, ax = plt.subplots()
+    ax.plot([0, 1], [0, 1], marker='o', color='0')
+    ax.plot([0, 1], [1, 0], marker='s', color='0.4')
+
+    # Sample parameters for the legend
+    modellabels = ['Model 1', 'Model 2']
+    msymbols = ['o', 's']
+    mcolor = ['0', '0.4']
+
+    # Call the function
+    ax = mdraw_legend(ax, None, modellabels, msymbols, mcolor)
+
+    # Assertions
+    legend = ax.get_legend()
+    assert legend is not None, "Legend was not created."
+
+    # Check number of legend entries
+    assert len(legend.get_texts()) == len(modellabels), "Incorrect number of legend entries."
+
+    # Check legend labels
+    for label, model_label in zip(legend.get_texts(), modellabels):
+        assert label.get_text() == model_label, "Legend labels do not match."
+
+    # Check legend marker colors and symbols
+    for line, symbol, color in zip(legend.legendHandles, msymbols, mcolor):
+        assert isinstance(line, Line2D), "Legend entry is not a Line2D instance."
+        assert line.get_marker() == symbol, "Legend marker symbol does not match."
+        assert line.get_color() == color, "Legend marker color does not match."
+
