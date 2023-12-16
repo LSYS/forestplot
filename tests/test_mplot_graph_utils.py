eaee867ceda5aa24d34571593aff96d887bf6815
LSYS <lucas@lucasshen.com>
2023-12-16 11:46:23 +0800

    Pleasing linters

--- tests/test_mplot_graph_utils.py
+++ tests/test_mplot_graph_utils.py
@@ -1,8 +1,8 @@
-from forestplot.mplot_graph_utils import mdraw_ref_xline, mdraw_yticklabels
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.pyplot import Axes
 
+from forestplot.mplot_graph_utils import mdraw_ref_xline, mdraw_yticklabels
 
 x, y = [0, 1, 2], [0, 1, 2]
 str_vector = ["a", "b", "c"]
@@ -23,24 +23,30 @@ input_df = pd.DataFrame(
 
 def test_mdraw_ref_xline():
     _, ax = plt.subplots()
-    ax = mdraw_ref_xline(ax, dataframe=input_df, model_col="yticklabel", annoteheaders=None, right_annoteheaders=None)
+    ax = mdraw_ref_xline(
+        ax,
+        dataframe=input_df,
+        model_col="yticklabel",
+        annoteheaders=None,
+        right_annoteheaders=None,
+    )
     assert isinstance(ax, Axes)
 
 
 def test_mdraw_yticklabels():
     # Prepare the input DataFrame
-    x = [0, 1, 2]
     str_vector = ["a", "b", "c"]
-    input_df = pd.DataFrame({
-        "yticklabel": str_vector,
-    })
+    input_df = pd.DataFrame(
+        {
+            "yticklabel": str_vector,
+        }
+    )
 
     # Create a matplotlib Axes object
     _, ax = plt.subplots()
 
     # Call the function
-    ax = mdraw_yticklabels(input_df, yticklabel='yticklabel',flush=True, ax=ax)
+    ax = mdraw_yticklabels(input_df, yticklabel="yticklabel", flush=True, ax=ax)
 
     assert isinstance(ax, Axes)
     assert [label.get_text() for label in ax.get_yticklabels()] == str_vector
-
