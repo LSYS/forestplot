33bf8d065a4c82f7ac63b17aadcd5b308c6ea7a8
Lucas Shen Y. S <lucas@lucasshen.com>
2023-12-23 21:31:01 +0800

    Add docstring & test for mplot (#88, #89) (#96)
    * Add docstring & test for mplot (#88, #89)
    
    * Pleasing linters

---
+++ tests/test_mplot.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from matplotlib.pyplot import Axes
+
+from forestplot import mforestplot
+
+dataname = "sleep-mmodel"
+data = f"https://raw.githubusercontent.com/lsys/forestplot/mplot/examples/data/{dataname}.csv"
+df = pd.read_csv(data)
+
+
+std_opts = dict(
+    dataframe=df,
+    estimate="coef",
+    ll="ll",
+    hl="hl",
+    varlabel="var",
+    model_col="model",
+)
+
+
+def test_vanilla_mplot():
+    ax = mforestplot(**std_opts)
+    assert isinstance(ax, Axes)
+
+    df_processed, ax = mforestplot(**std_opts, return_df=True)
+    assert isinstance(ax, Axes)
+    assert isinstance(df_processed, pd.DataFrame)
+
+
+def test_more_options():
+    df_processed, ax = mforestplot(
+        **std_opts,
+        color_alt_rows=True,
+        groupvar="group",
+        table=True,
+        rightannote=["var", "group"],
+        right_annoteheaders=["Variable", "Variable group"],
+        xlabel="Coefficient (95% CI)",
+        modellabels=["Have young kids", "Full sample"],
+        xticks=[-1200, -600, 0, 600],
+        return_df=True,
+        # Additional kwargs for customizations
+        **{
+            "marker": "D",  # set maker symbol as diamond
+            "markersize": 35,  # adjust marker size
+            "xlinestyle": (0, (10, 5)),  # long dash for x-reference line
+            "xlinecolor": "#808080",  # gray color for x-reference line
+            "xtick_size": 12,  # adjust x-ticker fontsize
+            "despine": False,
+        },
+    )
+    assert isinstance(ax, Axes)
+    assert isinstance(df_processed, pd.DataFrame)
